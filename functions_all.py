e2a6ff028ae68b7b4953f11df35c05e922e71abb
Rezink1n <84652452+Rezink1n@users.noreply.github.com>
2022-10-28 00:48:12 +0700

    Some clean

--- functions_all.py
+++ functions_all.py
@@ -1,18 +1,6 @@
 import random
 
 
-def place_finder(place, file):
-    i = 0
-    f = 0
-    while i < len(file):
-        if file[i][0] == place:
-            break
-        else:
-            i += 1
-            f += 1
-    return i, print(file[i])
-
-
 def make_step(place):
     d = random.randint(2, 12)
     place += d
@@ -21,9 +9,38 @@ def make_step(place):
     return place, d
 
 
+def place_type_is(place):
+    match place:
+        case 0: place_type = 'Start'
+        case 4: place_type = 'Taxes'
+        case 10: place_type = 'Prison'
+        case 12 | 28: place_type = 'Company'
+        case 20: place_type = 'Free parking'
+        case 30: place_type = 'Arrest'
+        case 38: place_type = '-100$'
+        case 5 | 15 | 25 | 35: place_type = 'Seaport'
+        case 2 | 17 | 33: place_type = 'Treasury'
+        case 7 | 22 | 36: place_type = 'Chance'
+        case _: place_type = 'Building'
+    return place_type
+
+
+def building_finder(place, file):
+    i = 0
+    while i < len(file):
+        if file[i][0] == place:
+            break
+        else:
+            i += 1
+    return i, print(file[i])
+
+
 def money_up(player, quantity):
     players[player][2] += quantity
 
 
 def money_down(player, quantity):
     players[player][2] -= quantity
+
+
+
--- test_zone.py
+++ test_zone.py
@@ -7,38 +7,19 @@ player_2 = ['Alex', 0, 1500, [0]]
 
 players = [player_refer, player_1, player_2]
 
-def buy_card(player, place):
+def buy_card(player, place, file):
     place_finder(place)
-    print('Money:', player_1[2])
     a = input('Do you want to buy it?\n >')
-    if a == 'yes':
-        player_1[2] -= cards_json[place_finder(place)[0]][3]
+    if a == 'y':
+        players[player][2] -= file[place_finder(place)[0]][3]
         print('You buy it! Congrats!')
         print('Money:', player_1[2])
-        player_1[3].append(cards_json[place_finder(place)[0]][0])
-    elif a == 'no':
+        player_1[3].append(file[place_finder(place)[0]][0])
+    elif a == 'n':
         print('You did not buy it :(')
-    elif a == 'show':
+    elif a == 's':
         print('You have:', player_1[3])
     return print('Next move!')
-
-
-def place_finder(place):
-    i = 0
-    f = 0
-    while i < len(cards_json):
-        if cards_json[i][0] == place:
-            break
-        else:
-            i += 1
-            f += 1
-    return i, print(cards_json[i])
-
-
-place = 10
-card_current = place_finder(place)
-
-print(card_current)
 
 # while x != 0:
 #     x = int(input('Place: '))
